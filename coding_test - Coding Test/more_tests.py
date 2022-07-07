7ec7c92bce51438c65cf610201348e23ce47c1d9
LDSA Machine User <machine@lisbondatascience.org>
2022-07-07 09:41:50 +0000

    add one more test to exercise 3

--- coding_test - Coding Test/more_tests.py
+++ coding_test - Coding Test/more_tests.py
@@ -1,81 +1,91 @@
 import numpy as np
 
 
-def test_exercise_2_I(read_cities):
-    cities_locations_data = read_cities('cities_locations_large.csv')
+def test_exercise_2_part_I(read_movies):
+    movies_data = read_movies('movies_large.csv')
+    
+    assert isinstance(movies_data, dict)
+    assert len(movies_data) == 200
 
-    assert len(cities_locations_data) == 15
-
-    assert 'Barcelona' in cities_locations_data
-    barcelona = cities_locations_data['Barcelona']
-    assert barcelona['idx'] == 10
-    assert barcelona['country'] == 'Spain'
-    np.testing.assert_almost_equal(barcelona['lat'], 41.3833, decimal=3)
-    np.testing.assert_almost_equal(barcelona['lng'], 2.1834, decimal=3)
-
-    assert 'Ottawa' in cities_locations_data
-    ottawa = cities_locations_data['Ottawa']
-    assert ottawa['idx'] == 2
-    assert ottawa['country'] == 'Canada'
-    np.testing.assert_almost_equal(ottawa['lat'], 45.4167, decimal=3)
-    np.testing.assert_almost_equal(ottawa['lng'], -75.7, decimal=3)
+    assert 'tt1375666' in movies_data
+    inception = movies_data['tt1375666']
+    assert inception["title"] == "Inception"
+    assert inception["year"] == 2010
+    np.testing.assert_almost_equal(inception["rating"], 8.8, decimal=1)
+    
+    assert 'tt0078748' in movies_data
+    alien = movies_data['tt0078748']
+    assert alien["title"] == "Alien"
+    assert alien["year"] == 1979
+    np.testing.assert_almost_equal(alien["rating"], 8.5, decimal=1)
 
 
-def test_exercise_2_II(create_dist_matrix):
-    cities_locations_data = {
-        'Andorra la Vella': {'idx': 0, 'lat': 42.5, 'lng': 1.5165, 'country': 'Andorra'},
-        'Toronto': {'idx': 1, 'lat': 43.7, 'lng': -79.42, 'country': 'Canada'},
-        'Ottawa': {'idx': 2, 'lat': 45.4167, 'lng': -75.7, 'country': 'Canada'},
-        'Paris': {'idx': 3, 'lat': 48.8667, 'lng': 2.3333, 'country': 'France'},
-        'Lyon': {'idx': 4, 'lat': 45.77, 'lng': 4.83, 'country': 'France'},
-        'Rome': {'idx': 5, 'lat': 41.896, 'lng': 12.4833, 'country': 'Italy'},
-        'Milan': {'idx': 6, 'lat': 45.47, 'lng': 9.205, 'country': 'Italy'},
-        'Lisbon': {'idx': 7, 'lat': 38.7227, 'lng': -9.1449, 'country': 'Portugal'},
-        'Porto': {'idx': 8, 'lat': 41.15, 'lng': -8.62, 'country': 'Portugal'},
-        'Madrid': {'idx': 9, 'lat': 40.4, 'lng': -3.6834, 'country': 'Spain'},
-        'Barcelona': {'idx': 10, 'lat': 41.3833, 'lng': 2.1834, 'country': 'Spain'},
-        'Washington': {'idx': 11, 'lat': 38.9047, 'lng': -77.0163, 'country': 'United States'},
-        'Atlanta': {'idx': 12, 'lat': 33.7627, 'lng': -84.4225, 'country': 'United States'},
-        'Harare': {'idx': 13, 'lat': -17.8178, 'lng': 31.0447, 'country': 'Zimbabwe'},
-        'Bulawayo': {'idx': 14, 'lat': -20.17, 'lng': 28.58, 'country': 'Zimbabwe'}
+def test_exercise_2_part_II(average_movie_rating):
+    movies_data = {
+        "tt0071411": {"title": "Dersu Uzala", "year": 1975, "rating": 8.2},
+        "tt0118799": {"title": "Life Is Beautiful", "year": 1997, "rating": 8.6},
+        "tt0252487": {"title": "The Chaos Class", "year": 1975, "rating": 9.3},
+        "tt0381681": {"title": "Before Sunset", "year": 2004, "rating": 8.1},
+        "tt0466460": {"title": "Khosla Ka Ghosla!", "year": 2006, "rating": 8.3},
+        "tt0075148": {"title": "Rocky", "year": 1976, "rating": 8.1},
+        "tt0263975": {"title": "The Girl with the Red Scarf", "year": 1977, "rating": 8.5},
+        "tt0109830": {"title": "Forrest Gump", "year": 1994, "rating": 8.8},
+        "tt1954470": {"title": "Gangs of Wasseypur", "year": 2012, "rating": 8.2},
+        "tt0074958": {"title": "Network", "year": 1976, "rating": 8.1},
+        "tt0363163": {"title": "Downfall", "year": 2004, "rating": 8.2},
+        "tt0347149": {"title": "Howl's Moving Castle", "year": 2004, "rating": 8.2},
+        "tt2375605": {"title": "The Act of Killing", "year": 2012, "rating": 8.2},
+        "tt0091763": {"title": "Platoon", "year": 1986, "rating": 8.1},
+        "tt0114787": {"title": "Underground", "year": 1995, "rating": 8.1},
+        "tt0214915": {"title": "Manichithrathazhu", "year": 1993, "rating": 8.7},
+        "tt6966692": {"title": "Green Book", "year": 2018, "rating": 8.2},
+        "tt3322420": {"title": "Queen", "year": 2013, "rating": 8.1},
+        "tt6316138": {"title": "Ayla: The Daughter of War", "year": 2017, "rating": 8.3},
+        "tt0468569": {"title": "The Dark Knight", "year": 2008, "rating": 9.0},
     }
-
-    dists = create_dist_matrix(cities_locations_data)
-
-    assert type(dists) == np.ndarray
-    assert dists.shape == (15, 15)
-
-    for i in range(dists.shape[0]):
-        assert dists[i][i] == 0
-        
-    np.testing.assert_almost_equal(dists[7][13], 7531.40, decimal=1)
-    np.testing.assert_almost_equal(dists[4][8], 1201.40, decimal=1)
-
-
-def test_exercise_2_III(furthest_cities):
-    np.random.seed(23)
-    dists = np.random.rand(10, 10) * 1000
-    for i in range(dists.shape[0]):
-        dists[i][i] = 0
-        for j in range(dists.shape[1]):
-            if j > i:
-                dists[i][j] = dists[j][i]
-
-    idx_1, idx_2 = furthest_cities(dists)
-    assert idx_1 == 5
-    assert idx_2 == 6
+                      
+    average_movie_rating_1976 = average_movie_rating(movies_data, 1976)
+    assert isinstance(average_movie_rating_1976, float)  
+    np.testing.assert_almost_equal(average_movie_rating_1976, 8.1, decimal=2)
+    
+    average_movie_rating_2004 = average_movie_rating(movies_data, 2004)
+    assert isinstance(average_movie_rating_2004, float)  
+    np.testing.assert_almost_equal(average_movie_rating_2004, 8.1666, decimal=2)
+    
+    average_movie_rating_2005 = average_movie_rating(movies_data, 2005)
+    assert isinstance(average_movie_rating_2005, float)  
+    np.testing.assert_almost_equal(average_movie_rating_2005, -1, decimal=2)
+    
+    average_movie_rating_2023 = average_movie_rating(movies_data, 2023)
+    assert isinstance(average_movie_rating_2023, float)  
+    np.testing.assert_almost_equal(average_movie_rating_2023, -1, decimal=2)
 
 
-def test_exercise_3_II(battle, Pokemon):
-    charmander = Pokemon(name='Charmander', max_health=25, speed=5)
-    charmander.level = 6
-    squirtle = Pokemon(name='Squirtle', max_health=30, speed=10)
-    squirtle.level = 5
-
-    np.random.seed(23)
-    winner, loser = battle(charmander, squirtle)
-
-    assert winner.name == "Squirtle"
-    assert loser.name == "Charmander"
-    assert winner.hp == 6
-    assert loser.hp == 0
+def test_exercise_2_part_III(pearson_correlation_year_rating):
+    
+    movies_data = {
+        "tt0071411": {"title": "Dersu Uzala", "year": 1975, "rating": 8.2},
+        "tt0118799": {"title": "Life Is Beautiful", "year": 1997, "rating": 8.6},
+        "tt0252487": {"title": "The Chaos Class", "year": 1975, "rating": 9.3},
+        "tt0381681": {"title": "Before Sunset", "year": 2004, "rating": 8.1},
+        "tt0466460": {"title": "Khosla Ka Ghosla!", "year": 2006, "rating": 8.3},
+        "tt0075148": {"title": "Rocky", "year": 1976, "rating": 8.1},
+        "tt0263975": {"title": "The Girl with the Red Scarf", "year": 1977, "rating": 8.5},
+        "tt0109830": {"title": "Forrest Gump", "year": 1994, "rating": 8.8},
+        "tt1954470": {"title": "Gangs of Wasseypur", "year": 2012, "rating": 8.2},
+        "tt0074958": {"title": "Network", "year": 1976, "rating": 8.1},
+        "tt0363163": {"title": "Downfall", "year": 2004, "rating": 8.2},
+        "tt0347149": {"title": "Howl's Moving Castle", "year": 2004, "rating": 8.2},
+        "tt2375605": {"title": "The Act of Killing", "year": 2012, "rating": 8.2},
+        "tt0091763": {"title": "Platoon", "year": 1986, "rating": 8.1},
+        "tt0114787": {"title": "Underground", "year": 1995, "rating": 8.1},
+        "tt0214915": {"title": "Manichithrathazhu", "year": 1993, "rating": 8.7},
+        "tt6966692": {"title": "Green Book", "year": 2018, "rating": 8.2},
+        "tt3322420": {"title": "Queen", "year": 2013, "rating": 8.1},
+        "tt6316138": {"title": "Ayla: The Daughter of War", "year": 2017, "rating": 8.3},
+        "tt0468569": {"title": "The Dark Knight", "year": 2008, "rating": 9.0},
+    }
+    
+    pearson_correlation = pearson_correlation_year_rating(movies_data)
+    assert isinstance(pearson_correlation, float)
+    np.testing.assert_almost_equal(pearson_correlation, -0.197, decimal=3)
